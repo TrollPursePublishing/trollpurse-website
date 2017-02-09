c9d696888829ea2787155aaef04fc88fa5811754
trollpurse <admin@trollpurse.com>
2017-02-09 18:23:21 +0000

    finalized gulp build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,35 @@
 var gulp = require('gulp');
-var minify = require('gulp-minify');
+var clean = require('del');
+var minifyJS = require('gulp-minify');
+var minifyHTML = require('gulp-htmlmin');
+var minifyIMG = require('gulp-imagemin');
 
-gulp.task('minify', function() {
-	
+gulp.task('cleanDest', function() {
+	return clean(['built/**/*']);
 });
 
-gulp.task('copy', function() {
+gulp.task('minifyHTML', function() {
+	return gulp.src('html/*.html')
+		.pipe(minifyHTML({collapseWhitespace: true}))
+		.pipe(gulp.dest('built/html/'));
 });
 
-gulp.task('deploy', function() {
+gulp.task('minifyJS', function() {
+	return gulp.src('js/*.js')
+		.pipe(minifyJS({
+			ext: {
+				src:'.js',
+				min: '.js'
+			}
+		}))
+		.pipe(gulp.dest('built/js/'));
 });
+
+gulp.task('minifyIMG', function() {
+	return gulp.src('images/*')
+		.pipe(minifyIMG())
+		.pipe(gulp.dest('built/images/'));
+		
+});
+
+gulp.task('build', ['cleanDest', 'minifyHTML', 'minifyJS', 'minifyIMG']);
