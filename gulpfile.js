b63178d35cb6acee8d7953e7681d02e106d0c454
trollpurse <admin@trollpurse.com>
2017-02-09 17:11:58 +0000

    gulp automation and adding of git ignore

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var minify = require('gulp-minify');
+
+gulp.task('minify', function() {
+	
+});
+
+gulp.task('copy', function() {
+});
+
+gulp.task('deploy', function() {
+});
