2fcace6ee163ffd3878013d7b14a8e39f6f69acb
Martin Hollstein <martin.hollstein@foreverinteractive.com>
2019-01-04 00:48:46 -0600

    something is wrong with the css

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ gulp.task('minifyHTML', function() {
 
 gulp.task('minifyCSS', function(){
 	return gulp.src(['css/*.css'])
-		.pipe(cleanCSS({compatibility: 'ie8'}))
+		// .pipe(cleanCSS({compatibility: 'ie8'})) TODO: it doesn't work in Chrome in prod
 		.pipe(gulp.dest('built/css'));
 });
 
